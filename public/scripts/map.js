a3dd826a5d3eadf060ea813bcab199dc8b7c1340
jbixon13 <jbixon13@gmail.com>
2020-01-03 01:42:49 -0500

    basic geolocation

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -24,3 +24,28 @@ xhr.onload = function() {
     }).addTo(map);
 };
 xhr.send();
+
+map.locate({
+    setView: true, maxZoom: 16
+});
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+    console.log(e);
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius, {
+        color:'black'
+    }).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+    console.log(e);
+}
+
+map.on('locationerror', onLocationError);
